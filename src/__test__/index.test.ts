21ab0e753f75f7b9c20ea24475adb7128c393da7
jtanudjaja <jonathantanudjaja@gmail.com>
2020-11-01 03:35:24 +0700

    v0.0.4

--- src/__test__/index.test.ts
+++
@@ -1,9 +0,0 @@
-import DokuflowClient from '../index';
-
-test('Create Client', () => {
-  const client = new DokuflowClient({
-    apiKey: 'test',
-    spaceName: 'test',
-  });
-  expect(client).toBeInstanceOf(DokuflowClient);
-});
